9446bf74d8e6939bc45b450c3e63491e8fbe9702
James <isd6542@outlook.com>
2026-01-25 18:31:28 +1100

    refactor/ move getManifest to handers/state.ts
    Moved the getManifest method to somewhere that made a little more sense.

--- plugin/handlers/state.ts
+++ plugin/handlers/state.ts
@@ -4,10 +4,51 @@ import {
 	InnerMessage,
 	TransportPacket,
 	Manifest,
-    SyncGroup
+    SyncGroup,
+    SharedItem
 } from "../types";
 import { sendSecureMessage } from "../networking";
 import { requestFile } from "./fileTransfer"
+import { download } from "../comm";
+import { decryptBinary } from "../crypto";
+
+
+export async function getManifest(
+	plugin: IOpVaultPlugin,
+	sharedItem: SharedItem,
+): Promise<Manifest | undefined> {
+	const buffer = await download(plugin, sharedItem.id, "manifest.json");
+	if (!buffer) {
+		console.error("[OPV] Failed to download manifest");
+		new Notice("Failed to download manifest. Check console for details.");
+		return;
+	}
+
+	const key =
+		sharedItem.pin && sharedItem.pin.length > 0 ? sharedItem.pin : null;
+	let manifestBuffer: Uint8Array | null = null;
+	if (key) {
+		manifestBuffer = await decryptBinary(buffer, key);
+	} else {
+		manifestBuffer = buffer;
+	}
+
+	if (!manifestBuffer) {
+		console.error("[OPV] Failed to decrypt manifest");
+		new Notice("Failed to decrypt manifest.");
+		return;
+	}
+
+	const manifestJSON = new TextDecoder().decode(manifestBuffer);
+	if (!manifestJSON) {
+		console.warn(`[OPV] Empty manifest JSON for ${sharedItem.id}`);
+		return;
+	}
+	const manifest = JSON.parse(manifestJSON) as Manifest;
+	console.debug(`[OPV] Received manifest for ${sharedItem.id}:`, manifest);
+	return manifest;
+}
+
 
 export function manifestUpdate(
 	plugin: IOpVaultPlugin,
